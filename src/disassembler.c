1a763db97745de8aa33056bbcb013c62ce3e943d
LacksalCB <3.14159pii@gmail.com>
2026-09-16 15:23:40 -0400

    Adding some more instructions

--- src/disassembler.c
+++ src/disassembler.c
@@ -5,12 +5,11 @@
  * Stolen from objdump, linear sweep through each symbol is probably the best balance of speed and complexity
  * Can run a clean-up pass to ensure all code is reached and that nothing major is missed
  */
-
 typedef enum {
     F_NONE = 0,
     F_MODRM = 1 << 0,
     F_IMM8 = 1 << 1,
-    F_IMM16 = 1 << 2,
+    F_IMM32 = 1 << 2,
     F_GROUP = 1 << 3
 }flags;
 
@@ -21,43 +20,134 @@ typedef struct {
 
 const char mod_table[4][16] = {"INDIRECT", "DISP_1B", "DISP_4B", "REG_MODE"};
 
-const char reg[3][4] = {"eax", "ecx", "edx"};
+const char reg[8][6] = {"%eax", "%ecx", "%edx", "%ebx", "%esp", "%ebp", "%esi", "%edi"};
 
 const isa_entry_t isa_table[256] = {
-    [0x01] = {"add", F_NONE},
-    [0xc3] = {"ret", F_NONE}
+    [0x01] = {"add", F_MODRM},
+    [0x55] = {"push\t%rbp", F_NONE},
+    [0x48] = {"", F_GROUP},
+    [0x89] = {"mov", F_MODRM}, 
+    [0x8d] = {"lea", F_MODRM},
+    [0xc3] = {"ret", F_NONE},
+    [0xc7] = {"movl", F_IMM32|F_MODRM}
 };
 
 void disassemble(unsigned char* buff, header_t* header) {
-    int offsets[128];
+    int offsets[256];
     int idx = 0;
     for (int i = 0; i < header->symtab->size; i++) {
         if ((header->symtab[i].symtab_entry->section == header->sections[3].sh_entry->shstrtab_idx) ||
                 (header->symtab[i].symtab_entry->section == header->sections[4].sh_entry->shstrtab_idx) ||
                     (header->symtab[i].symtab_entry->section == header->sections[5].sh_entry->shstrtab_idx)) {
-            //printf("0x%02lx: %s\n", header->symtab[i].symtab_entry->offset, header->symtab[i].symtab_entry->name);
             offsets[idx] = i;
-            printf("%d, %d\n", idx, i);
             idx++;
         }
     }
-  
+
     for (int i = 0; i < idx; i++) {
-        printf("%ld\n", header->sections[offsets[idx]].sh_entry->offset);
-        for (int j = header->sections[offsets[idx]].sh_entry->offset; j < header->sections[offsets[idx]].sh_entry->size; j++) {
-           printf("0x%01x", buff[j]); 
-        } 
+        uint64_t j = header->symtab[offsets[i]].symtab_entry->offset;
+        printf("\n%016lx: <%s>\n", j, header->symtab[offsets[i]].symtab_entry->name);
+        while (buff[j] != 0x00) {
+            unsigned char opcode = buff[j];
+            
+            // Ret kinda has to be handled separately just to end linear search through symbol
+            if (opcode == 0xc3) { // ret                 
+                printf("%01x\t, %s", opcode, isa_table[opcode].mnemonic);
+                break;
+            }
+            
+            // Single byte instructions
+            if (isa_table[opcode].flags == F_NONE) { 
+                printf("    %lx:    %x\t\t%s\n", j, opcode, isa_table[opcode].mnemonic);
+                j++;
+                continue;
+            }
+
+            unsigned char instr[8] = {0, 0, 0, 0, 0, 0, 0};
+
+            // For Prefixes
+            if (isa_table[opcode].flags == F_GROUP) {
+                instr[0] = buff[j];
+                if (isa_table[buff[j+1]].flags == F_MODRM) {
+                    instr[1] = buff[j+1];
+                    instr[2] = buff[j+2];
+                    unsigned char mod = (instr[2] & 0b11000000) >> 6;
+                    switch(mod) {
+                        case 0b00:
+                            break;
+                        case 0b01:
+                            break;
+                        case 0b10:
+                            break;
+                        case 0b11:    
+                            unsigned char s = (instr[2] & 0b00111000) >> 3;
+                            unsigned char d = instr[2] & 0b00000111;
+                            printf("    %lx:    ", j);
+                            for (int k = 0; k < 3; k++) {
+                                printf("%x ", instr[k]);
+                            } 
+                            printf("\t%s\t%s,%s\n", isa_table[instr[1]].mnemonic, reg[s], reg[d]);
+                            j += 2;
+                            break;
+                    }
+                    j++;
+                    continue;
+                }
+            } 
+
+            // c7 45 f4 ef be 00 00
+            // c7 -> mov with 32 bit imm
+            // 45 -> 0b01 000101
+            // f4 -> 0b11 110 100
+            // Multi byte instructions
+            if (isa_table[opcode].flags == (F_IMM32|F_MODRM)) {
+                instr[0] = buff[j];
+                instr[1] = buff[j+1];
+                unsigned char mod = (instr[1] & 0b11000000) >> 6;
+                switch(mod) {
+                    case 0b00:
+                        break;
+                    case 0b01:
+                        unsigned char rm = instr[1] & 0b00000111;
+                        char disp = buff[j+2];
+                        instr[2] = disp;
+                        unsigned char val_bytes[4];
+                        memcpy(val_bytes, buff+j+3, sizeof(val_bytes));
+                        for (int k = 3; k < 7; k++) {
+                            instr[k] = buff[j+k];
+                        }
+                        unsigned char tmp = 0;
+                        tmp = val_bytes[0];
+                        val_bytes[0] = val_bytes[3];
+                        val_bytes[3] = tmp;
+                        tmp = val_bytes[1];
+                        val_bytes[1] = val_bytes[2];
+                        val_bytes[2] = 1;
+
+                        uint64_t imm = uctoull(val_bytes, sizeof(val_bytes));
+                        printf("    %lx:    ", j);
+                        for (int k = 0; k < 7; k++) {
+                            printf("%02x ", instr[k]);
+                        }
+                        printf("\t%s\t$0x%04lx,%01x(%s)", isa_table[instr[0]].mnemonic, imm, disp, reg[rm]);
+                        break;
+                    case 0b10:
+                        break;
+                    case 0b11:    
+                        unsigned char s = (instr[2] & 0b00111000) >> 3;
+                        unsigned char d = instr[2] & 0b00000111;
+                        printf("    %lx:    ", j);
+                        for (int k = 0; k < 3; k++) {
+                            printf("%x ", instr[k]);
+                        } 
+                        printf("\t%s\t%s,%s\n", isa_table[instr[1]].mnemonic, reg[s], reg[d]);
+                        j += 2;
+                        break;
+                }
+                j++;
+                continue;              
+            }
+            j++;
+        }    
     }
-
-    // Print out the add instruction in main:
-    print_bytes(buff, 0x1131, 0x1132);
-
-    //0x1131 0x1132
-    unsigned char opcode = buff[0x1131];
-    unsigned char modrm = buff[0x1132];
-    unsigned char mod = (modrm & 0b11000000) >> 6;
-    unsigned char s = (modrm & 0b00111000) >> 3;
-    unsigned char d = modrm & 0b00000111;
-
-    printf("0x1131: 0x%01x 0x%01x:\t %s \t %s, %s", opcode, modrm, isa_table[opcode].mnemonic, reg[s], reg[d]);
 }
